bb2c395e81ba09839dae639f8f5133d5e5799947
thinkAmi <dev.thinkami+git@gmail.com>
2015-03-10 09:30:17 +0900

    first commit

---
+++ Handheld/MainActivity.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+using Android.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.OS;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+using Android.Util;
+using Android.Gms.Common.Apis;
+using Android.Gms.Wearable;
+
+namespace Handheld
+{
+    [Activity(Label = "Handheld", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity,
+    IResultCallback,
+    IGoogleApiClientConnectionCallbacks,
+    IGoogleApiClientOnConnectionFailedListener,
+    IMessageApiMessageListener 
+    {
+        Android.Gms.Common.Apis.IGoogleApiClient client;
+        private const string Tag = "HandheldActivity";
+        private const string MessageTag = "gdgshinshu";
+
+        public void OnConnected(Android.OS.Bundle connectionHint)
+        {
+            Android.Gms.Wearable.WearableClass.MessageApi.AddListener(client, this);
+        }
+
+        public void OnConnectionSuspended(int cause)
+        {
+        }
+
+        public void OnConnectionFailed(Android.Gms.Common.ConnectionResult result)
+        {
+        }
+
+        public void OnResult(Java.Lang.Object result)
+        {
+        }
+
+        public void OnMessageReceived(IMessageEvent e)
+        {
+            if (MessageTag.Equals(e.Path))
+            {
+                var month = System.Text.Encoding.UTF8.GetString(e.GetData());
+                Log.Debug(Tag, month);
+
+                SendResponseAsync(int.Parse(month));
+            }
+        }
+
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            client = new GoogleApiClientBuilder(this)
+                .AddApi(Android.Gms.Wearable.WearableClass.Api)
+                .AddConnectionCallbacks(this)
+                .AddOnConnectionFailedListener(this)
+                .Build();
+            client.Connect();
+        }
+            
+
+        public ICollection<string> NodeIds
+        {
+            get
+            {
+                var results = new HashSet<string>();
+                var nodes =
+                    WearableClass.NodeApi.GetConnectedNodes(client).Await().JavaCast<INodeApiGetConnectedNodesResult>();
+
+                foreach (var node in nodes.Nodes)
+                {
+                    results.Add(node.Id);
+                }
+                return results;
+            }
+        }
+
+        public async void SendResponseAsync(int month)
+        {
+            var restClient = new RestSharp.RestClient("http://ringo-tabetter-api.herokuapp.com/");
+            var request = new RestSharp.RestRequest("api/v1/month", RestSharp.Method.GET);
+
+            var response = await restClient.ExecuteTaskAsync(request);
+            RestSharp.Deserializers.JsonDeserializer deserial = new RestSharp.Deserializers.JsonDeserializer();
+            var results = deserial.Deserialize<List<RingoByMonths>>(response);
+
+            var topRingo = results.OrderByDescending(r => r.quantities[month]).First();
+
+            this.RunOnUiThread(() =>
+                {
+                    var status = FindViewById<TextView>(Resource.Id.HandheldStatus);
+                    status.Text = topRingo.name;
+                }
+            );
+
+            var msg = (month + 1).ToString() + ":" + topRingo.name + ":" + topRingo.color;
+
+
+            var nodeIds = await Task.Run(() => NodeIds);
+            foreach (var nodeId in nodeIds)
+            {
+                WearableClass.MessageApi.SendMessage(client, nodeId, MessageTag, System.Text.Encoding.UTF8.GetBytes(msg));
+            }
+        }
+
+        public class RingoByMonths
+        {
+            public string name { get; set; }
+            public List<int> quantities { get; set; }
+            public string color { get; set; }
+        }
+    }
+}
---
+++ Wear/MainActivity.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Wearable.Views;
+using Android.Widget;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Android.Util;
+using Android.Gms.Common.Apis;
+using Android.Gms.Wearable;
+using Android.Graphics; 
+
+namespace Wear
+{
+    [Activity(Label = "Wear", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity,
+    IResultCallback,
+    IGoogleApiClientConnectionCallbacks,
+    IGoogleApiClientOnConnectionFailedListener,
+    IMessageApiMessageListener 
+    {
+        Android.Gms.Common.Apis.IGoogleApiClient client;
+        private const string Tag = "WearActivity";
+        private const string MessageTag = "gdgshinshu";
+
+        public void OnConnected(Android.OS.Bundle connectionHint)
+        {
+            Android.Gms.Wearable.WearableClass.MessageApi.AddListener(client, this);
+        }
+
+        public void OnConnectionSuspended(int cause)
+        {
+        }
+
+        public void OnConnectionFailed(Android.Gms.Common.ConnectionResult result)
+        {
+        }
+
+        public void OnResult(Java.Lang.Object result)
+        {
+        }
+
+        public void OnMessageReceived(IMessageEvent e)
+        {
+            if (MessageTag.Equals(e.Path))
+            {
+                var msg = System.Text.Encoding.UTF8.GetString(e.GetData());
+                Log.Debug(Tag, msg);
+
+                var ringo = msg.Split(':');
+                var colorName = System.Drawing.Color.FromName(ringo[2]);
+                var colorRgb = Android.Graphics.Color.Rgb(colorName.R, colorName.G, colorName.B);
+
+                this.RunOnUiThread(() =>
+                    {
+                        var layout = FindViewById<LinearLayout>(Resource.Id.linear);
+                        layout.SetBackgroundColor(colorRgb);
+
+                        var status = FindViewById<TextView>(Resource.Id.status);
+                        status.Text = string.Format("{0}月 - {1}", ringo[0], ringo[1]);
+                        status.SetBackgroundColor(colorRgb);
+                        status.SetTextColor(Color.Black);
+                    });
+            }
+        }
+
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            client = new GoogleApiClientBuilder(this)
+                .AddApi(Android.Gms.Wearable.WearableClass.Api)
+                .AddConnectionCallbacks(this)
+                .AddOnConnectionFailedListener(this)
+                .Build();
+            client.Connect();
+
+            var v = FindViewById<WatchViewStub>(Resource.Id.watch_view_stub);
+            v.LayoutInflated += delegate
+                {
+                    var send = FindViewById<Button>(Resource.Id.send);
+                    send.Click += (sender, e) =>
+                        {
+                            var month = new System.Random().Next(0, 11);
+                            SendMessageAsync(month);
+                        };
+                };
+        }
+
+
+        public async void SendMessageAsync(int month)
+        {
+            var nodeIds = await Task.Run(() => NodeIds);
+
+            foreach (var nodeId in nodeIds)
+            {
+                WearableClass.MessageApi.SendMessage(client, nodeId, MessageTag, Encoding.UTF8.GetBytes(month.ToString()));
+            }
+        }
+
+        public ICollection<string> NodeIds
+        {
+            get
+            {
+                var results = new HashSet<string>();
+                var nodes =
+                    WearableClass.NodeApi.GetConnectedNodes(client).Await().JavaCast<INodeApiGetConnectedNodesResult>();
+
+                foreach (var node in nodes.Nodes)
+                {
+                    results.Add(node.Id);
+                }
+                return results;
+            }
+        }
+    }
+}
+
+
+
